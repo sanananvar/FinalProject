5f27991a674a9f19a080a0b2f3ca06c9d4dad65d
senan elekberov <senanaal@code.edu.az>
2025-02-23 23:57:59 +0400

    FinalProject Admin Page and sime bugfixes version

--- Kaffein-Ui/src/mocks/FooterNavData.js
+++
@@ -1,6 +0,0 @@
-export const footerNav = [
-    { name: "Contact", href: "/" },
-    { name: "Location", href: "/" },
-    { name: "Career", href: "/career" },
-  ];
-  
--- Kaffein-Ui/src/mocks/NavData.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const navMenu = [
-  { name: "Menu", href: "/menu" },
-  { name: "Philosophy", href: "/about" },
-  { name: "Interior", href: "/interior" },
-  { name: "Location", href: "/Location" },
-];
